626d82dc967daa79c7f94bf747dd030576af45b1
Ricardo Mestre <ricardomestre@ricardos-air.lan>
2022-05-26 23:06:13 +0100

    cra boilerplate cleanup and basic routing

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+// import { ChakraProvider, theme } from '@chakra-ui/react';
+// import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Posts from './modules/Posts';
+import Welcome from './modules/Welcome';
+import Post from './modules/Post';
 
-function App() {
+const App = () => {
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="post/:id" element={<Post />} />
+      </Routes>
+    </BrowserRouter>
+    // <ChakraProvider theme={theme}>{/* <ColorModeSwitcher /> */}</ChakraProvider>
   );
-}
+};
 
 export default App;
